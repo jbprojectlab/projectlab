2474995705f7dd7a0f3cbfeec290847e88d75bc2
James Byrd <James.Byrd1265@gmail.com>
2018-10-29 03:28:59 -0400

    style: about me

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
 import {Name} from './name'
 import {Quote} from './quote'
+import {About} from './about'
 
 export default class Main extends Component {
   render () {
@@ -9,6 +10,7 @@ export default class Main extends Component {
       <div id='main'>
         <Name />
         <Quote />
+        <About />
       </div>
     )
   }
