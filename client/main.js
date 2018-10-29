d09e42e98fc39997d7d57090a833e29ee134a980
James Byrd <James.Byrd1265@gmail.com>
2018-10-29 00:43:59 -0400

    feat: about

--- client/main.js
+++ client/main.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
-import {NavBar} from './navbar'
+import {Name} from './name'
+import {Quote} from './quote'
 
 export default class Main extends Component {
   render () {
     return (
       <div id='main'>
-        <NavBar />
+        <Name />
+        <Quote />
       </div>
     )
   }
