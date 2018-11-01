4fa0a832fb6df006c36c69577fcfdbc4f517f253
James Byrd <James.Byrd1265@gmail.com>
2018-11-01 14:28:00 -0400

    fix: quote

--- client/quote.js
+++ client/quote.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Quote = props => {
   return (
     <div id='quote-container'>
-      <h2 id='quote'>"The world is but a canvas to the imagination"</h2>
+      <h2 id='quote'>"The world is but a canvas to our imagination"</h2>
       <h4 id='author'>-Henry David Thoreau</h4>
     </div>
   )
