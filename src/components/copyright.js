7d37e103d8f27f37aaf6ded471bfb9a9e95a4555
James Byrd <James.Byrd1265@gmail.com>
2019-07-04 14:54:20 -0400

    chore: update copyright

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -4,7 +4,7 @@ import '../css/copyright.css'
 export const Copyright = props => {
   return (
     <div id='copyright-nav'>
-      <h6 id='copyright-text'>© jbprojectlab.com</h6>
+      <h6 id='copyright-text'>© jamesbyrd.dev</h6>
     </div>
   )
 }
